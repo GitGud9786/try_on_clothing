2b5763d0ca83c293891e10256ec6d883a8e5a053
Shahir Awlad <121874937+Shahir-Awlad@users.noreply.github.com>
2026-08-21 00:47:14 +0600

    Modify training parameters in train.sh
    Updated training script parameters for dataset and run configuration.

--- train.sh
+++ train.sh
@@ -18,20 +18,41 @@
 # %env CUDA_VISIBLE_DEVICES=0
 # %env PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 #CODE FOR TRAINING
-python train.py \
+# python train.py \
+#   --config_name VITONHD \
+#   --data_root_dir OverSampler \
+#   --save_root_dir logs \
+#   --save_name micro_test_run \
+#   --batch_size 1 \
+#   --accum_iter 4 \
+#   --img_H 512 \
+#   --img_W 384 \
+#   --max_epochs 200 \
+#   --valid_epoch_freq 1 \
+#   --logger_freq 10 \
+#   --precision 16 \
+#   --resume_path ckpts/VITONHD_PBE_pose.ckpt \
+#   --vae_load_path ckpts/VITONHD_VAE_finetuning.ckpt \
+#   --no_strict_load \
+#   --no_validation
+
+
+  python train.py \
   --config_name VITONHD \
-  --data_root_dir OverSampler \
+  --data_root_dir my_dataset \
   --save_root_dir logs \
-  --save_name micro_test_run \
+  --save_name run_clean_captions \
   --batch_size 1 \
-  --accum_iter 4 \
+  --accum_iter 1 \
   --img_H 512 \
   --img_W 384 \
   --max_epochs 200 \
-  --valid_epoch_freq 1 \
   --logger_freq 10 \
   --precision 16 \
   --resume_path ckpts/VITONHD_PBE_pose.ckpt \
   --vae_load_path ckpts/VITONHD_VAE_finetuning.ckpt \
   --no_strict_load \
-  --no_validation
+  --no_validation \
+  --require_captions \
+  --main_unet_unfreeze_interval 5 \
+  --main_unet_unfreeze_lr 1e-6
